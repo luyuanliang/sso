4b86d741f7a1251e2d290b34ea426c3dd528c510
luyuanliang <luyuanliang_1984@163.com>
2021-10-25 22:09:32 +0800

    add demo.

--- sso-server/src/main/java/org/web/common/sso/server/SSOServerApplication.java
+++ sso-server/src/main/java/org/web/common/sso/server/SSOServerApplication.java
@@ -1,13 +1,14 @@
 package org.web.common.sso.server;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
+
+
 @SpringBootApplication()
 //@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
-@MapperScan(basePackages = {"org.web.common.sso.server.dao"})
+//@MapperScan(basePackages = {"org.web.common.sso.server.dao"})
 @ImportResource({"classpath:mapper/mybatis-config-all.xml"})
 public class SSOServerApplication {
 
